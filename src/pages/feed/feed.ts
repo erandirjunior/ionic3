67e9eab04a501f9228822b157ccfeef269e81993
Erandir Junior <aefs12junior@gmail.com>
2017-08-19 11:45:01 -0300

    Iniciando com TypeScript: criando variável e fazendo interpolação.

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public nomeUsuario:string = "Erandir Junior";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
